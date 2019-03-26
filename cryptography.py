d13872735001620809c0ba19cc6d929b3ecaf189
Spyglass117 <46973734+Spyglass117@users.noreply.github.com>
2019-03-26 07:43:04 -0400

    Update cryptography.py

--- cryptography.py
+++ cryptography.py
@@ -10,3 +10,18 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+test = 0
+
+while test == 0:
+    command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    
+    if command == "q":
+        test = test + 1
+        print("Goodbye!")
+    elif command == "e":
+        message = input("Message: ")
+        key = input("Key: ")
+    elif command == "d":
+        pass
+    else:
+        print("Did not understand command, please try again.")
