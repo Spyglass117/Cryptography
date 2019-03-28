bb2b894d2e71989806975fb2212f5e5a65611691
Spyglass117 <46973734+Spyglass117@users.noreply.github.com>
2019-03-28 14:53:43 -0400

    Update cryptography.py

--- cryptography.py
+++ cryptography.py
@@ -11,16 +11,29 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 test = 0
+str1 = ""
 
 while test == 0:
     command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-    
+    n = 0
     if command == "q":
         test = test + 1
         print("Goodbye!")
     elif command == "e":
         message = input("Message: ")
         key = input("Key: ")
+        while len(key) < len(message):
+            key += key
+        associations += associations
+        l = len(message)
+        while n < l:
+            numrepm = associations.find(message[n])
+            numrepk = associations.find(key[n])
+            finalnum = numrepm + numrepk
+            letrep = associations[finalnum]
+            str1 = str1 + letrep
+            n = n + 1
+        print(str1)
     elif command == "d":
         pass
     else:
